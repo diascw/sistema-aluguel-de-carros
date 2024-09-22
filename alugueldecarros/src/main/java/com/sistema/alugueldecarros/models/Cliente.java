5b57e1a666fbcf54762939a418b24f1c2025d6cc
LeandraCR <lee.ramss15@gmail.com>
2024-09-22 18:05:36 -0300

    Implementação do CRUD de pedidos e ajustes no layout

--- alugueldecarros/src/main/java/com/sistema/alugueldecarros/models/Cliente.java
+++ alugueldecarros/src/main/java/com/sistema/alugueldecarros/models/Cliente.java
@@ -1,6 +1,7 @@
 package com.sistema.alugueldecarros.models;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "clientes")
@@ -12,7 +13,21 @@ public class Cliente {
 
     private String nome;
     private String cpf;
+    private String rg;
     private String endereco;
+    private String profissao;
+
+    @ElementCollection
+    @CollectionTable(name = "empregadores", joinColumns = @JoinColumn(name = "cliente_id"))
+    @Column(name = "entidade_empregadora")
+    private List<String> entidadesEmpregadoras;
+
+    @ElementCollection
+    @CollectionTable(name = "rendimentos", joinColumns = @JoinColumn(name = "cliente_id"))
+    @Column(name = "rendimento")
+    private List<Double> rendimentos;
+
+    // Getters e Setters
 
     public Long getId() {
         return id;
@@ -38,11 +53,43 @@ public class Cliente {
         this.cpf = cpf;
     }
 
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
     public String getEndereco() {
         return endereco;
     }
 
     public void setEndereco(String endereco) {
         this.endereco = endereco;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
+
+    public List<String> getEntidadesEmpregadoras() {
+        return entidadesEmpregadoras;
+    }
+
+    public void setEntidadesEmpregadoras(List<String> entidadesEmpregadoras) {
+        this.entidadesEmpregadoras = entidadesEmpregadoras;
+    }
+
+    public List<Double> getRendimentos() {
+        return rendimentos;
+    }
+
+    public void setRendimentos(List<Double> rendimentos) {
+        this.rendimentos = rendimentos;
     }
 }
